de0fc462cda9bb9bf6b5ecedaac4bdcd8abb0925
Klainer00 <166978145+Klainer00@users.noreply.github.com>
2025-05-26 00:46:31 -0400

    ini

---
+++ src/main/java/com/perfulandia/post_venta/PostVentaApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.post_venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostVentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/post_venta/PostVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.post_venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
